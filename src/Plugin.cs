92e14a918fa5665125cd3e87c51a439bafc4a3ef
Gaia <Gaiaalexf@gmail.com>
2026-03-12 03:06:35 -0700

    fuck coding, but I have to now </3 it dont work yet and I cant build the plugin yet but hopefully it dont complain ingame rn additionally some other random minor changes removed the public license thing, its not correct for this

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using BepInEx;
-using UnityEngine;
+﻿using BepInEx;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 using SlugBase.Features;
+using System;
+using UnityEngine;
 using static SlugBase.Features.FeatureTypes;
 
-namespace SlugTemplate
-    //none of this does anything right now I think so dont worry about this file 
-
-
+namespace Gaia
 {
 
     [BepInPlugin(MOD_ID, "Gaia Slugcat Real", "1.5.1")]
@@ -16,21 +15,35 @@ namespace SlugTemplate
     {
         private const string MOD_ID = "Gaia.GaiaScug";
 
+
         // Add hooks
         public void OnEnable()
         {
             On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
 
             // Put your custom hooks here!
-     
+            IL.Player.Grabability += Player_Grabability1;
+
         }
-        
+
         // Load any resources, such as sprites or sounds
         private void LoadResources(RainWorld rainWorld)
-        {
+        {             
+
         }
 
-
+        private void Player_Grabability1(ILContext il)
+        {
+            ILCursor c = new(il);
+            for (int i = 0; i < 2; i++) c.GotoNext(MoveType.After, x => x.MatchIsinst(nameof(Centipede)));
+            c.GotoNext(MoveType.After, x => x.MatchCallvirt(typeof(Centipede).GetMethod("get_Small")));
+            c.Emit(OpCodes.Ldarg_0);
+            c.EmitDelegate(CentiGrab);
+        }
+        private static bool CentiGrab(bool can, Player self)
+        {
+            return can || self.SlugCatClass == SlugcatStats.Name.Gaia;  // replace with slugcats name
+        }
 
 
     }
