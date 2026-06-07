add91c1db89339b35fb4c741750c2eaa37403d66
Gaia <gaiaalexf@gmail.com>
2026-06-07 04:49:07 -0700

    fix protorot in garbage wastes probably hopefully

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -4,6 +4,8 @@ using MonoMod.Cil;
 using SlugBase.Features;
 using System;
 using UnityEngine;
+using MoreSlugcats;
+using RWCustom;
 using static SlugBase.Features.FeatureTypes;
 
 namespace Gaia
@@ -22,16 +24,32 @@ namespace Gaia
             On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
 
             // Put your custom hooks here!
-            
+            On.PlacedObject.FilterData.FromString += FilterData_FromString_TimelineFix;
 
 
         }
-
+        public const string GaiaID = "Gaia"; // whatever it's supposed to be
+        public static SlugcatStats.Name GaiaEnumName {get; private set;} // making it somewhat read only but not really
+        private static void FilterData_FromString_TimelineFix(On.PlacedObject.FilterData.orig_FromString orig, PlacedObject.FilterData self, string s)
+        {
+            orig(self, s);
+    
+            if (!self.availableToPlayers.Contains(MoreSlugcatsEnums.SlugcatStatsName.Gourmand) && self.availableToPlayers.Contains(GaiaEnumName))
+            {
+                self.availableToPlayers.Remove(GaiaEnumName); 
+            }
+        }
+        
         // Load any resources, such as sprites or sounds
         private void LoadResources(RainWorld rainWorld)
         {
-
+            GaiaEnumName = new SlugcatStats.Name(GaiaID, false); // would recommend to initiate it.
         }
+        
+        
+        
+        
+        
         
         
     }
